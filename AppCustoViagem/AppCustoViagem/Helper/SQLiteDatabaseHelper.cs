12fced2860d42f7201a596ceae4a1b03ff67b6e1
Usuario <Usuario@CPD03-M15>
2022-09-06 12:22:07 -0300

    Adicionar arquivos de projeto.

---
+++ AppCustoViagem/AppCustoViagem/Helper/SQLiteDatabaseHelper.cs
@@ -0,0 +1,43 @@
+﻿using AppCustoViagem.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+
+namespace AppCustoViagem.Helper
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+
+            _conn.CreateTableAsync<Pedagio>().Wait();
+
+        }
+
+
+        public void insert(Pedagio p)
+        {
+
+        }
+        public void update(Pedagio p)
+        {
+
+        }
+
+        public Task<List<Pedagio>> getAll()
+        {
+            return _conn.Table<Pedagio>().ToListAsync();
+        }
+
+        public Task<int> delete(int id)
+        {
+            return _conn.Table<Pedagio>().DeleteAsync(i => i.Id == id);
+        }
+    }
+}
---
+++ AppCustoViagem/AppCustoViagem/Model/Pedagio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace AppCustoViagem.Model
+{
+    public class Pedagio
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Localizacao { get; set; }
+        public double Valor { get; set; }
+    }
+}
